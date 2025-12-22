4058fcb4c6e797f68d81a7d23e73ba37d28875fa
TrishaPal2003 <your-email@example.com>
2025-12-22 10:09:17 +0600

    backend changed

--- backend/user_app/models/__init__.py
+++ backend/user_app/models/__init__.py
@@ -2,4 +2,5 @@ from .user import User
 from .location import Location
 from .accounts import Account
 
+
 __all__ = ["User", "Location", "Account"]
--- backend/user_app/serializers/account_serializer.py
+++ backend/user_app/serializers/account_serializer.py
@@ -1,25 +1,18 @@
 from rest_framework import serializers
-from ..models import Account, User
-from .location_serializer import LocationSerializer
+from ..models.user import User
+from ..models.accounts import Account
+from ..serializers.location_serializer import LocationSerializer
 
-class UserSerializer1(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
 
 class AccountSerializer(serializers.ModelSerializer):
-    # user = serializers.StringRelatedField() 
-    user = UserSerializer1()
     location = LocationSerializer(read_only=True)
 
     class Meta:
         model = Account
-        fields = [
-            "id",
-            "user",
-            "blood_group",
-            "adress",
-            "last_donation_date",
-            "is_available",
-            "location",
-        ]
+        fields = ["address", "blood_group", "last_donation_date", "is_available", "location"]
+
+
+class UserSerializerPublic(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "role"]
